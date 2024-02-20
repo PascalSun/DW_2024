069ded5f1d74691a7d28ca89b6c63a192e537fa0
pascalsun <pascalsun23@gmail.com>
2024-02-20 14:22:47 +0800

    update, give up csv for the sql server, doing another way

--- OLTP/sqlserver/install.sh
+++ OLTP/sqlserver/install.sh
@@ -11,12 +11,3 @@ echo $SERVER
 /opt/mssql-tools/bin/sqlcmd -S $SERVER -U SA -P $SA_PASSWORD -Q "CREATE DATABASE AdventureworksDW;"
 /opt/mssql-tools/bin/sqlcmd -S $SERVER -U SA -P $SA_PASSWORD -Q "CREATE DATABASE AdventureworksDWDemo;"
 
-# Assuming the .sql files are adapted for SQL Server and placed in the respective directories
-# Import the .sql files into the respective databases
-
-cd /data/AdventureWorkDWDemo
-
-/opt/mssql-tools/bin/sqlcmd -S $SERVER -U SA -P $SA_PASSWORD -d AdventureworksDWDemo -i ./CreateTables.sql
-/opt/mssql-tools/bin/sqlcmd -S $SERVER -U SA -P $SA_PASSWORD -d AdventureworksDWDemo -i ./InsertData.sql
-#/opt/mssql-tools/bin/sqlcmd -S $SERVER -U SA -P $SA_PASSWORD -d AdventureworksDWDemo -i /data/AdventureWorkOLTP/instawdb.sql
-#/opt/mssql-tools/bin/sqlcmd -S $SERVER -U SA -P $SA_PASSWORD -d AdventureworksDWDemo -i /data/AdventureWorkDW/instawdbdw.sql
